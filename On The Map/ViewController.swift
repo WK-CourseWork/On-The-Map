7ce9e45ca7904aa09fdc6e48d25614acc95c57c2
Justin Kumpe <jakumpe@kumpes.com>
2022-07-22 14:55:59 -0500

    Initial Commit

---
+++ On The Map/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  On The Map
+//
+//  Created by Justin Kumpe on 7/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
